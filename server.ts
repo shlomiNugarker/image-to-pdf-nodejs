f9e77b98c843d5e2bf485bbd68c8665b87cdf0d9
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-19 11:46:44 +0300

    d

--- server.ts
+++ server.ts
@@ -42,10 +42,6 @@ app.use(cors()) // This will allow all origins. You can configure it to allow sp
 
 app.use('/api/ocr', ocrRoutes)
 
-app.get('/**', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
 const PORT = process.env.PORT || 3030
 http.listen(PORT, () => {
   console.log(`⚡️Server is running on port: ${PORT}`)
