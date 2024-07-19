538e447a6991ad0416ecc164da884dfc94a2a8bc
Shlomi Nugarker <shlomin1231@gmail.com>
2024-07-19 10:29:36 +0300

    remove  code

--- server.ts
+++ server.ts
@@ -26,19 +26,15 @@ app.use(session)
 app.use(json())
 app.use(express.static('public'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-  const corsOptions = {
-    origin: [
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-      'https://image-to-pdf-free.vercel.app/',
-    ],
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'https://image-to-pdf-free.vercel.app/',
+  ],
+  credentials: true,
 }
+app.use(cors(corsOptions))
 
 app.use('/api/ocr', ocrRoutes)
 
